e3472d4c4bf6edb2b665dfa8fa2ca4fab66c51d9
Christo <chrisjosephfitz@gmail.com>
2026-02-11 00:19:54 +0000

    add Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Chris Fitzgerald | Portfolio",
   description: "Frontend engineer portfolio of Chris Fitzgerald.",
+    icons: {
+        icon: "/favicon.png",
+    },
 };
 
 export default function RootLayout({
